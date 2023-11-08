7b06853b5f98b2e6f6bbdc02e60051964a02483e
Alessandro Rossi <rossisverige@yahoo.se>
2023-11-08 12:45:31 +0000

    Saving

--- my-app/src/components/ControlledForm.js
+++ my-app/src/components/ControlledForm.js
@@ -1,12 +1,35 @@
 import React, { Component } from 'react'
 
 export class ControlledForm extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            name: ""
+        }
+    }
+
+    handleNameChange = (event) => {
+        this.setState(
+            {
+                name: event.target.value
+            },
+        );
+
+    }
+    
     render() {
         return (
             <div>
                 <form>
                     <label htmlFor="id-name">Your Name</label>
-                    <input id="id-name" name="name" type="text" />
+                    <input 
+                        value={this.state.name}
+                        onChange={this.handleNameChange} 
+                        id="id-name"
+                        name="name"
+                        type="text" 
+                    />
                     <input type="Submit" value="Submit" />
                 </form>
                 
